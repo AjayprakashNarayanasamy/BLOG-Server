dd0e971fcb4283532ab9cfcfbf8cae79ef09db1d
AjayPrakash06 <ajay.prakash@agilisium.com>
2024-03-17 14:32:15 +0530

    API call

--- database/index.js
+++ database/index.js
@@ -1,18 +1,49 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize("Blog", "ajayprakashn66", "2RBoXu9bagnM", {
-    host: "ep-fragrant-king-257539.ap-southeast-1.aws.neon.tech",
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: true, // or "require"
-    },
-  });
-  const testDbConnection = async () => {
-    try {
-      await sequelize.authenticate();
-      console.log("Connection has been established successfully.");
-    } catch (error) {
-      console.error("Unable to connect to the database:", error);
-    }
-  };
-  module.exports = { sequelize, testDbConnection };
+// const sequelize = new Sequelize("Blog", "ajayprakashn66", "2RBoXu9bagnM", {
+//     host: "ep-fragrant-king-257539.ap-southeast-1.aws.neon.tech",
+//     dialect: "postgres",
+//     dialectOptions: {
+//       ssl: true, // or "require"
+//     },
+//   });
+//   const testDbConnection = async () => {
+//     try {
+//       await sequelize.authenticate();
+//       console.log("Connection has been established successfully.");
+//     } catch (error) {
+//       console.error("Unable to connect to the database:", error);
+//     }
+//   };
+//   module.exports = { sequelize, testDbConnection };
+const {Client} = require('pg');
+
+const client = new Client({
+  user:'ajayprakashn66',
+  password:'2RBoXu9bagnM',
+  host:'ep-fragrant-king-257539.ap-southeast-1.aws.neon.tech',
+  port:5432,
+  database:'Blog',
+  ssl: {
+    // Specify SSL mode
+    rejectUnauthorized: true, // This is usually used for development purposes, it skips SSL certificate validation
+    // Other SSL options can be specified here if necessary
+  },
+  sslmode: 'require',
+})
+let connection
+async function dbConnection()
+{
+  try {
+
+    connection = await client.connect()
+    console.log('Connected to postgres')
+    
+  } catch (error) {
+    console.log('Error connecting to PostgreSQL database', error);
+  }
+}
+module.exports={
+  client,
+  dbConnection
+}
--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require("express")
-const databse = require("./database/index")
+const { client, dbConnection} = require("./database/index")
 const app = express()
 const port = 8000 || process.env.PORT
 app.use(express.json())
-app.get("/",(req,res,next)=>{
-    res.send("AjayTest")
+app.get("/",async(req,res,next)=>{
+    try {
+        // Here you can make database queries since the connection is ensured
+        // For example, query all users from the database
+        const users = await client.query('SELECT * FROM users');
+        res.json(users.rows);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ error: 'Error fetching users' });
+      }
 })
-app.get("/users",(req,res,next)=>{
-    res.send("Ajay")
-})
-databse.testDbConnection()
-app.listen(8000,()=>console.log(`Port successfully listening on ${port}`))
+// app.get("/users",(req,res,next)=>{
+//     res.send("Ajay")
+// })
+dbConnection()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  })
+  .catch(error => {
+    console.error('Failed to start server:', error);
+    process.exit(1); // Exit the process with a non-zero status code
+  });
